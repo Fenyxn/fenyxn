517dedc70086d900074a7be38d4442782b2456d2
Akupowar2403 <powarakanksha9188@gmail.com>
2026-05-22 13:42:18 +0530

    feat : website deployment

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isCustomDomain = process.env.CUSTOM_DOMAIN === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: isCustomDomain ? "" : "/fenyxn",
+  trailingSlash: true,
 };
 
 export default nextConfig;
